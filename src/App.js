dad9c247bed23d3dd231d01e969b2f652ac8e860
manoj lingabathula <manojl0430@gmail.com>
2024-09-02 21:13:59 +0530

    Add increase cart count

--- src/App.js
+++ src/App.js
@@ -1,13 +1,77 @@
+import { Component } from "react";
 import MovieList from "./MovieList";
 import Navbar from "./Navbar";
+import { Movies } from "./MoviesData";
 
-function App() {
-  return (
-    <>
-      <Navbar />
-      <MovieList />
-    </>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: Movies,
+      cartCount:0
+    };
+  }
+  handleAddStars = (movie) => {
+    const { movies } = this.state;
+    let mid = movies.indexOf(movie);
+
+    if (movies[mid].star < 5) {
+      movies[mid].star += 0.5;
+    }
+    this.setState({
+      movies,
+    });
+  };
+
+  //   Handle DecStars
+  handleDecStars = (movie) => {
+    const { movies } = this.state;
+    let mid = movies.indexOf(movie);
+    if (movies[mid].star > 0) {
+      movies[mid].star -= 0.5;
+    }
+    this.setState({
+      movies,
+    });
+  };
+  // handleFav
+  handleToggleFav = (movie) => {
+    const { movies } = this.state;
+    let mid = movies.indexOf(movie);
+    movies[mid].fav = !movies[mid].fav;
+    this.setState({
+      movies,
+    });
+  };
+ 
+  handleToggleCart = (movie) => {
+    let { movies ,cartCount} = this.state;
+    let mid = movies.indexOf(movie);
+    movies[mid].isInCart = !movies[mid].isInCart;
+    if(movies[mid].isInCart){
+      cartCount+=1;
+    }else{
+      cartCount-=1;
+    }
+    this.setState({
+      movies,cartCount
+    });
+  };
+  render() {
+    const { movies ,cartCount} = this.state;
+    return (
+      <>
+        <Navbar cartCount={cartCount}/>
+        <MovieList
+          movies={movies}
+          addStars={this.handleAddStars}
+          decStars={this.handleDecStars}
+          handleFav={this.handleToggleFav}
+          handleCart={this.handleToggleCart}
+        />
+      </>
+    );
+  }
 }
 
 export default App;
--- src/Navbar.js
+++ src/Navbar.js
@@ -6,16 +6,17 @@ import NavbarCSS from "./Navbar.module.css"
 
 class Navbar extends Component {
   render() {
+    const {cartCount}=this.props;
     return (
       <>
         <div className={NavbarCSS.nav}>
-          <div className={NavbarCSS.title}> title</div>
+          <div className={NavbarCSS.title}> Movie-App</div>
           <div className={NavbarCSS.cartContainer}>
             <img
               src="https://cdn-icons-png.flaticon.com/128/891/891462.png"
               alt="cart icon" className={NavbarCSS.cart}
             />
-            <span className={NavbarCSS.cartCount} >0</span>
+            <span className={NavbarCSS.cartCount} >{cartCount}</span>
           </div>
         </div>
       </>
